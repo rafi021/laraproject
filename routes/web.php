5d408c6c081d91543187f99fe58d121da8c82592
rafi021 <mahmud.ibrahim021@gmail.com>
2022-06-27 11:17:05 +0600

    understanding controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrontController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,102 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function (Request $request) {
+Route::get('/', [FrontController::class, 'home'])->name('home');
 
+Route::get('/about-us', [FrontController::class, 'about'])->name('about');
 
-    // dd(
-    //     //"Laravel 9"
-    //     $request->path(),  // path name
-    //     $request->is('/'), // 1 0r 0
+Route::get('/contact-page', [FrontController::class, 'contact'])->name('contact');
 
-    //     $request->fullUrl(),
-    //     $request->host(),
-    //     $request->httpHost(),
-    //     $request->schemeAndHttpHost(),
+Route::get('/service-page', [FrontController::class, 'service'])->name('service');
 
-    //     $request->routeIs('home'),  // 1 or 0
-    //     $request->header('X-Header-Name'),
-    //     $request->header('X-Header-Name', 'default'),
-    //     $request->bearerToken(),  // Used in API building
-
-    //     $request->ip(),
-    //     $request->prefers(['text/html', 'application/json']),
-    // );
-
-    // $data = [
-    //     'page_name' => 'Home Page',
-    //     'name' => 'Larael 9 Course'
-    // ];
-
-    // return response($data)
-    // ->header('Content-Type', 'application/json')
-    // ->cookie('My_IDCard', 'Mahmud Ibrahim', 3600);
-
-    //dd($request->collect());
-
-    return view('home', [
-        'page_name' => 'Home Page',
-        'name' => 'Larael 9 Course'
-    ]);
-
-
-})->name('home')->middleware('auth');
-
-Route::get('login', function(){
-    return "Login";
-})->name('login');
-
-Route::get('/about-us', function () {
-    return view('about', [
-        'page_name' => 'About Page',
-    ]);
-})->name('about');
-
-Route::get('/contact-page', function () {
-    $page_name = "Contact Page";
-    $products=[
-       1 => [
-        'name' => 'Bag',
-        'color' => 'Red',
-        'price' => '1200',
-       ],
-       2 => [
-        'name' => 'Sunglass',
-        'color' => 'Black',
-        'price' => '550',
-       ],
-       3 => [
-        'name' => 'BodySpray',
-        'color' => 'Blue',
-        'price' => '850',
-       ],
-    ];
-    $product_count = count($products);
-
-    return response()->json([
-        'products' => $products,
-        'product_count' => $product_count,
-    ], 200);
-    // return view('contact', compact(
-    //     'page_name',
-    //     'product_count',
-    //     'products'
-    // ));
-})->name('contact');
-
-Route::get('/service-page', function () {
-
-    $services = [
-        'Web Design',
-        'Web Development',
-        'App Development',
-        'Graphics Design',
-    ];
-
-
-    return view('service', compact('services'));
-})->name('service');
-
-
+Route::get('/send-me-details', [FrontController::class, 'sendMeDetails'])->name('sendmedetails');
 
 
