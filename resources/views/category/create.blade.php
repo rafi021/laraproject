55437eb56fd1d2ea6fe214f7be1fdfea0ce5033f
rafi021 <mahmud.ibrahim021@gmail.com>
2022-07-02 21:25:04 +0600

    Session overview and Flash Messages using session

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -6,6 +6,13 @@
 
 <div class="row">
     <div class="col-8 m-auto">
+
+        @if (session('status'))
+            <div class="bg-success text-white">
+                {{ session('status') }}
+            </div>
+        @endif
+
         <form action="{{ route('category.store') }}" method="POST">
             @csrf
             <div class="mb-3">
@@ -22,18 +29,7 @@
                     </span>
                 @enderror
             </div>
-            <div class="mb-3">
-                <label for="category-slug" class="form-label">Category Slug</label>
-                <input type="text" class="form-control @error('category_slug')
-                is-invalid
-                @enderror" id="category-slug"
-                name="category_slug"  placeholder="Please provide Category slug">
-                @error('category_slug')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
-            </div>
+
             <div class="form-check mb-3">
                 <input class="form-check-input" type="checkbox"
                 name="is_active" id="flexCheckDefault">
