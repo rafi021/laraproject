4343265b7896c4c1657899575c636c2fc6881b9b
rafi021 <mahmud.ibrahim021@gmail.com>
2022-07-06 23:10:04 +0600

    Versioned Assets (Cache Improvements)

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
     @stack('admin_style')
 
@@ -23,7 +23,7 @@
         @yield('content')
     </div>
 
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
     @stack('admin_script')
   </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,7 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .postCss('resources/css/app.css', 'public/css')
     .sourceMaps();
+
+if(mix.inProduction()){
+    mix.version();
+}
