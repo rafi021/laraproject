f1c03d71512c9626734579fbd24e5e8764218a17
rafi021 <mahmud.ibrahim021@gmail.com>
2022-07-09 11:18:11 +0600

    Faker - fake data generation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Country;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
